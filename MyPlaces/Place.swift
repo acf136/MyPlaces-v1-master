cce8e138d0003e7f8cf02f7137287033f2254593
acf136 <acf136@acf136s-mac.local>
2018-10-29 14:24:03 +0100

    Added Crop background view image in cell && Round cell corners

--- MyPlaces/Place.swift
+++ MyPlaces/Place.swift
@@ -8,6 +8,16 @@
 
 import MapKit
 
+extension UIImage {
+    func cropped(boundingBox: CGRect) -> UIImage? {
+        guard let cgImage = self.cgImage?.cropping(to: boundingBox) else {
+            return nil
+        }
+        
+        return UIImage(cgImage: cgImage)
+    }
+}
+
 class Place {
 
     // We could have created a PlaceType.swift file for this enumeration or just put it in current
--- MyPlaces/PlaceCell.swift
+++ MyPlaces/PlaceCell.swift
@@ -12,12 +12,17 @@ class PlaceCell: UITableViewCell {
 
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView! {
+        didSet {
+            backgroundImageView.layer.cornerRadius = 20
+        }
+    }
     
     // Set data of the place in the cell IBOutlets
     func bind(place: Place?) {
         nameLabel.text = place?.name
         idLabel.text = place?.id
-        backgroundImageView.image = place?.image
+        // Crop image to adapt to table cell size
+        backgroundImageView.image = place?.image?.cropped(boundingBox : CGRect(origin: CGPoint(x:0,y:0) , size: CGSize(width:375,height:150)) )
     }
 }
