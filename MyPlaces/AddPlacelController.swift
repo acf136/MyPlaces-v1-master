09ec3586d9c5d84df91d2553dc2bcbee2eead2ed
acf136 <acf136@acf136s-mac.local>
2018-10-30 19:56:31 +0100

    AddPlaceController layout to standard values

--- MyPlaces/AddPlacelController.swift
+++ MyPlaces/AddPlacelController.swift
@@ -11,6 +11,7 @@ import MapKit
 
 class AddPlaceController: UIViewController {
     var place: Place?
+    let locationNew = CLLocationCoordinate2D(latitude: Double.random(in: 1...360) - 90.0, longitude: Double.random(in: 1...360) - 180.0)
     let manager = PlaceManager.shared
     var tbv : UITableView!
     
@@ -25,27 +26,31 @@ class AddPlaceController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        place = Place(type: .generic ,name: "Enter a name", description: "Enter a description", image_in: nil , www: nil )
-        place?.location =  CLLocationCoordinate2D(latitude: Double.random(in: 1...360) - 90.0, longitude: Double.random(in: 1...360) - 180.0)
-        locationPlace.text = String(format: "Latitude: %3.2f Longitude: %3.2f", arguments: [(place?.location.latitude)!, (place?.location.longitude)!])
+        locationPlace.text = String(format: "Latitude: %3.2f Longitude: %3.2f", arguments: [(locationNew.latitude), (locationNew.longitude)])
         typeLabelPlace.text = "Select Type of place"
         //"\(place?.type ?? .generic))"
-        nameEditPlace.text = place?.name
+        nameEditPlace.text = "Enter a name"
         descLabelPlace.text = "Description of place"
-        descEditPlace.text = place?.description
+        descEditPlace.text = "Enter a description"
         
         saveButton.layer.cornerRadius = 10
         cancelButton.layer.cornerRadius = 10
     }
     
-    @IBAction func savePlace(_ sender: Any) {
-        if place != nil { manager.append(place!) }
-        tbv.reloadData()
-        dismiss(animated: true, completion: nil)
+    @IBAction func cancelAdd(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Cancel" {
+            dismiss(animated: true, completion: nil)
+        }
     }
 
-    @IBAction func cancel(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    @IBAction func savePlace(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Save" {
+            place = Place(type: .generic ,name: nameEditPlace.text!, description: descEditPlace.text! , image_in: nil , www: nil )
+            place?.location = locationNew
+            manager.append(place!)
+            tbv.reloadData()
+            dismiss(animated: true, completion: nil)
+        }
+        
     }
-    
 }
