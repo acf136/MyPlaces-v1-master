cd8da2d8a20f47588ad64a15849a149933ae9bf7
acf136 <acf136@acf136s-mac.local>
2018-11-02 20:06:42 +0100

    Scale to Fill / No horizontal scrolling

--- MyPlaces/PlaceDetailViewController.swift
+++ MyPlaces/PlaceDetailViewController.swift
@@ -28,8 +28,8 @@ class PlaceDetailViewController: UIViewController {
         idOfPlace.text = place.id
         typeOfPlace.text = "\(place.type)"
         typeOfPlace.textColor = manager.itemTypeColor(place.id) //UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
-        //descrOfPlace.text = place.description
-        descrOfPlace.text = " bla bla lba bla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lba "
+        descrOfPlace.text = place.description
+        //descrOfPlace.text = " bla bla lba bla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lbabla bla lba "
         descrOfPlace.sizeToFit()
         
         imageOfPlace.image = place.image
