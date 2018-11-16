0c0a7c75e98a629e90cee63bf674be8e9362b8e4
acf136 <acf136@acf136s-mac.local>
2018-11-16 17:59:50 +0100

    After MVC project reorganisation in folders

---
+++ MyPlaces/Map/C/PlaceMapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaceMapViewController.swift
+//  MyPlaces
+//
+//  Created by acf136 on 16/11/2018.
+//  Copyright © 2018 Albert Mata Guerra. All rights reserved.
+//
+
+import UIKit
+
+class PlaceMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
