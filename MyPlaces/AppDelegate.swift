51b09bed18119e3793a9d2151566af5177c11594
acf136 <acf136@acf136s-mac.local>
2018-11-22 15:05:58 +0100

    Problem with "Tab bar item" and "Bar item" solved

--- MyPlaces/AppDelegate.swift
+++ MyPlaces/AppDelegate.swift
@@ -23,18 +23,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let filePath = docsPath.appendingPathComponent(manager.nameOfFileJSON())  // This is the name of JSON file where we load/save the places in every session
         
          //If JSON files containing Place's then append to manager
-//        do {
-//            let jsonData = try Data(contentsOf: filePath)
-//            let placesJSON = manager.placesFrom(jsonData: jsonData)
-//            manager.insertJSONIntoPlaces(placesJSON: placesJSON)
-//            print("Loaded JSON data of Places")
-//        } catch {
+        do {
+            let jsonData = try Data(contentsOf: filePath)
+            let placesJSON = manager.placesFrom(jsonData: jsonData)
+            manager.insertJSONIntoPlaces(placesJSON: placesJSON)
+            print("Loaded JSON data of Places")
+        } catch {
 //        // else generate n random places and append to manager
             manager.append( Place(id: "1", type: .generic, locationName: "Plaza Cataluña 1, Barcelona 08002, Spain", myDescription: "The very center of Barcelona, the most global and cosmopolitan town in Europe." , coordinate: CLLocationCoordinate2D(latitude: 41.385910, longitude: 2.169540), www: "", image: UIImage(named:"PlazaCatalunya1"),  title: "Plaça Catalunya", discipline: "") )
             manager.append( Place(id: "2", type: .touristic, locationName: "Park Güell , Barcelona 08024, Spain", myDescription: "You can't forget to visit this emblematic place in Barcelona" , coordinate: CLLocationCoordinate2D(latitude: 41.415830, longitude: 2.148610), www: "", image: UIImage(named:"ParcGüell1"),  title: "Parc Güell", discipline: "") )
             
             print("Generated JSON data of Places")
-//        }
+        }
     
         return true
     }
